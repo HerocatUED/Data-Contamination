54e283d155047c52e0bace0453026c35d94b3254
haox <2100013152@stu.pku.edu.cn>
2024-06-02 13:15:50 +0800

    debug

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import numpy as np
+import torch
 from tqdm import tqdm
 from sklearn.metrics import roc_auc_score
 from utils import *
@@ -10,14 +11,18 @@ def inference(models, data):
     Output: probability of dirty.
     """
     model_0, tokenizer_0, model_1, tokenizer_1 = models
-    n, m, threshold = 100, 10, 0.8
+    n, m, threshold = 10, 10, 0.8
     result = np.zeros(len(data))
+    with open("loss_log.txt", "w", encoding='utf-8') as f:
+        f.write('loss log\n')
     for i in tqdm(range(len(data))):
         neighbours = neighbourhood_generation(model_0, tokenizer_0, data[i], n, m, threshold)
-        assert len(neighbours) < n
-        loss = loss_func(data[i], model_1, tokenizer_1)
-        mean_loss = np.mean(loss_func(neighbours, model_1, tokenizer_1))
+        assert len(neighbours) <= n
+        loss = loss_func(tokenizer_1(data[i],return_tensors='pt').input_ids.to(device), model_1)
+        mean_loss = loss_func(torch.tensor([tokenizer_1.convert_tokens_to_ids(neighbour) for neighbour in neighbours],device=device), model_1)
         # TODO
+        with open("loss_log.txt", "a", encoding='utf-8') as f:
+            f.write(f'index: {i} loss: {loss} mean neighbour loss: {mean_loss}\n')
         if loss - mean_loss < - 0.2:
             result[i] = 1
     return result
@@ -33,22 +38,24 @@ def main():
     print("Validating...")
     with open("dataset/valid.json", "r", encoding='utf-8') as f:
         val_data = json.load(f)
-        label = val_data[:]['label'] == 'dirty'
+        val_data=val_data[:50]+val_data[-50:]
+        label = [data['label']=='dirty' for data in val_data]
         label = np.array(label)
-        predict = inference(models, val_data[:]['text'])
+        predict = inference(models, [data['text'] for data in val_data])
         score = roc_auc_score(label, np.hstack(predict, 1-predict))
         print(score)
     # test
     print('testing')
     with open("dataset/test.json", "r", encoding='utf-8') as f:
         test_data = json.load(f)
-        predict = inference(test_data[:]['text'])
+        test_text = [data['text'] for data in test_data]
+        predict = inference(models, test_text)
     with open("dataset/output.json", "w", encoding='utf-8') as f:
-        output = list({'text': 'none', 'score': 'none'}) * len(test_data)
-        for i, data, score in enumerate(zip(test_data[:]['text'], predict)):
+        output = [{'text': 'none', 'score': 'none'}] * len(test_data)
+        for i, data, score in enumerate(zip(test_text, predict)):
             output[i]['text'] = data
             output[i]['score'] = score
-        json.dump(output)
+        json.dump(output, f)
             
             
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -6,34 +6,38 @@ import torch.nn.functional as F
 from transformers import BertTokenizer, BertForMaskedLM, AutoTokenizer, AutoModelForCausalLM
 
 os.environ['HF_ENDPOINT'] = "https://hf-mirror.com"
-
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+EPS = 1e-6
 def load_neighbour_model():
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    model = BertForMaskedLM.from_pretrained('bert-base-uncased')
+    model = BertForMaskedLM.from_pretrained('bert-base-uncased').to(device)
     model.eval()
     return model, tokenizer
 
 
 def load_detect_model():
     tokenizer = AutoTokenizer.from_pretrained("detected_model")
-    model = AutoModelForCausalLM.from_pretrained("detected_model", torch_dtype=torch.bfloat16)
+    model = AutoModelForCausalLM.from_pretrained("detected_model", torch_dtype=torch.bfloat16).to(device)
     return model, tokenizer
 
 
-def loss_func(input_text, model, tokenizer):
+def loss_func(input_ids, model):
     loss = 0
-    for text in input_text:
-        inputs = tokenizer(text, return_tensors='pt')
-        outputs = model(**inputs, labels=inputs['input_ids'])
-        loss = outputs.loss.item()
-    return loss / len(input_text)
+    for id in input_ids:
+        # inputs = tokenizer(text, return_tensors='pt')
+        outputs = model(input_ids=id[None,:], labels=id[None,:])
+        loss += outputs.loss.item()
+    return loss / len(input_ids)
 
 
 def neighbourhood_generation(model, tokenizer, text, n, m, threshold=0.30):
     # Tokenize input text
     inputs = tokenizer(text, return_tensors='pt')
-    input_ids = inputs.input_ids
-    tokens = tokenizer.convert_ids_to_tokens(input_ids[0])
+    input_ids = inputs.input_ids.to(device)
+    # TODO: split tokens in more reasonable way
+    raw_tokens = tokenizer.convert_ids_to_tokens(input_ids[0])
+    tokens, success_tokens = raw_tokens[:500], raw_tokens[500:]
+    input_ids = input_ids[:, :500]
 
     # Get embeddings and apply dropout
     with torch.no_grad():
@@ -41,7 +45,7 @@ def neighbourhood_generation(model, tokenizer, text, n, m, threshold=0.30):
 
     # Compute swap probabilities
     def swap_probability(p_word, p_swap_word):
-        return p_swap_word / (1 - p_word)
+        return p_swap_word / (1 - p_word+EPS)
 
     # Find top m replacements for each token
     top_replacements = []
@@ -54,15 +58,17 @@ def neighbourhood_generation(model, tokenizer, text, n, m, threshold=0.30):
             probs = F.softmax(outputs, dim=-1)
 
         p_word = probs[0, i, input_ids[0, i]].item()
-        swap_probs = []
+        # swap_probs = []
 
-        for j in range(probs.size(-1)):
-            if j != input_ids[0, i].item():
-                p_swap_word = probs[0, i, j].item()
-                swap_probs.append((j, swap_probability(p_word, p_swap_word)))
-
-        swap_probs.sort(key=lambda x: x[1], reverse=True)
-        top_replacements.append((i, swap_probs[0]))
+        # for j in range(probs.size(-1)):
+        #     if j != input_ids[0, i].item():
+        #         p_swap_word = probs[0, i, j].item()
+        #         swap_probs.append((j, swap_probability(p_word, p_swap_word)))
+        #
+        # swap_probs.sort(key=lambda x: x[1], reverse=True)
+        probs[0, i, input_ids[0, i].item()]=0
+        max_j = torch.argmax(probs[0, i]).item()
+        top_replacements.append((i,(max_j, swap_probability(p_word,probs[0,i,max_j].item()))))
 
     # Generate neighbors
     neighbors = []
@@ -73,7 +79,7 @@ def neighbourhood_generation(model, tokenizer, text, n, m, threshold=0.30):
         replacements = random.choices(top_replacements, k=m)
         for k, (j, _) in replacements:
             new_text[k] = tokenizer.convert_ids_to_tokens(j)
-        neighbors.append(new_text)
+        neighbors.append(new_text+success_tokens)
 
     return neighbors
 
